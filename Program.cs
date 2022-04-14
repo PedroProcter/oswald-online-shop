18eb1a52185c02630d7623b490f0583628a09cf4
Albert2707 <202010652@itla.edu.do>
2022-04-14 19:01:47 -0400

    Adding service to slider

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using oswald_online_shop.Data;
-
+using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
